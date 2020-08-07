d0d8a7db1a69405c0320be991efb0d561c166eef
ReadMost <rauan.amangeldiyev@nu.edu.kz>
2020-08-07 13:41:06 +0600

    rollback to working state

--- db/models.py
+++ db/models.py
@@ -1,14 +1,12 @@
 # coding=utf-8
 import datetime
 
-from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey, types
+from sqlalchemy import Column, String, Integer, Float, DateTime, ForeignKey
 
 from db.base import Base, session
 from geoalchemy2 import Geometry
 import pytz
-
 timezone = pytz.timezone("Asia/Almaty")
-
 
 class BlackBoxPacket(Base):
 	__tablename__ = 'telematics_blackboxpacket'
@@ -25,35 +23,17 @@ class BlackBoxPacket(Base):
 		session.commit()
 		return self
 
-
-class TimeStamp(types.TypeDecorator):
-	impl = types.DateTime
-	LOCAL_TIMEZONE = datetime.datetime.utcnow().astimezone().tzinfo
-
-	def process_bind_param(self, value: datetime, dialect):
-		if value.tzinfo is None:
-			value = value.astimezone(self.LOCAL_TIMEZONE)
-
-		return value.astimezone(timezone.utc)
-
-	def process_result_value(self, value, dialect):
-		if value.tzinfo is None:
-			return value.replace(tzinfo=timezone.utc)
-
-		return value.astimezone(timezone.utc)
-
-
 class ShortPacket(Base):
 	__tablename__ = 'telematics_shortpacket'
 
 	id = Column(Integer, primary_key=True, autoincrement=True)
-	date_time = Column(TimeStamp, nullable=True)
+	date_time = Column(DateTime, nullable=True)
 	point = Column(Geometry('POINT'), nullable=True)
 	speed = Column(Integer, nullable=True)
 	course = Column(Integer, nullable=True)
 	alt = Column(Integer, nullable=True)
 	sats = Column(Integer, nullable=True)
-	created_at = Column(TimeStamp, default=datetime.datetime.now(timezone))
+	created_at = Column(DateTime, default=datetime.datetime.now(timezone))
 	imei = Column(String, nullable=True)
 	black_box = Column('black_box_id', Integer, ForeignKey('telematics_blackboxpacket.id'))
 
@@ -66,7 +46,6 @@ class ShortPacket(Base):
 		self.sats = sats
 		self.black_box = black_box
 		self.imei = imei
-
 
 class ExtendedPacket(Base):
 	__tablename__ = 'telematics_extendedpacket'
@@ -87,7 +66,6 @@ class ExtendedPacket(Base):
 		self.ibutton = ibutton
 		self.short_packet = short_packet
 
-
 class Params(Base):
 	__tablename__ = 'telematics_params'
 
@@ -102,3 +80,5 @@ class Params(Base):
 		self.type = type
 		self.value = value
 		self.extended = extended
+
+
