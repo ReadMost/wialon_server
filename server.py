6f81e43052edb2be972c0cb18455d44dd6a879af
ReadMost <rauan.amangeldiyev@nu.edu.kz>
2020-08-24 20:34:56 +0600

    added custom imei to recive info REVERT CHANGES

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ class ClientThread(MyThread):
 	def __init__(self, clientAddress, clientsocket):
 		super().__init__()
 		self.csocket = clientsocket
-		self.imei = '866795031232745'
-		self.is_authorised = [True]
+		self.imei = None
+		self.is_authorised = [False]
 		self.clientAddress = clientAddress
 		print("New connection added: ", clientAddress)
 
